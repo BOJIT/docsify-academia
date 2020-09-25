a839a1f6f07944de25a5763b9841f1b91853beb0
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-09-25 18:06:37 +0100

    blank template for developing new plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'docsify-katex.js',
+    filename: 'docsify-academia.js',
     path: path.resolve(__dirname, 'dist')
   },
   mode: 'development'
