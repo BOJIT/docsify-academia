e2e724059024cde86f8cf41b330a0066f1d5de6a
BOJIT <35117353+BOJIT@users.noreply.github.com>
2020-09-27 21:40:34 +0100

    successfully finds all headings and adds numeric string - now needs numbering system

--- src/index.js
+++ src/index.js
@@ -10,8 +10,23 @@ let blockOptions = {
 (function () {
   function install(hook) {
     hook.beforeEach(content => {
-      return content;
+      const tokens = marked.lexer(content);
+      var markdown = [];
+      var index = 0;
+      tokens.forEach(element => {
+        if(element.type === "heading") {
+          // Find location of corresponding header
+          var heading_position = content.indexOf(element.raw, index);
+          // Push preceeding markdown onto array
+          markdown.push(content.slice(index, heading_position));
+          // Push new header onto array
+          markdown.push(element.raw.replace('# ', '# 1.5.6 '))
+          index = heading_position + element.raw.length;
+        }
+      });
+      return markdown.join("");
     });
+
     hook.afterEach(function (html, next) {
       next(html);
     });
